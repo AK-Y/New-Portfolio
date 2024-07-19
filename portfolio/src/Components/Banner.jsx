a3733b171e4d79cde6846c71a90184ca36fd1e16
ARUN KUMAR YADAV <91981223+AK-Y@users.noreply.github.com>
2024-07-20 04:15:35 +0530

    Update Banner.jsx

--- portfolio/src/Components/Banner.jsx
+++ portfolio/src/Components/Banner.jsx
@@ -13,7 +13,7 @@ const Banner = () => {
     {
       text: "Full Stack Developer.",
       className:
-        "dark:text-teal-600 text-teal-600 sm:text-teal-600 sm:text-4xl md:text-5xl text-2xl",
+        "dark:text-teal-600 md:text-teal-600 md:dark:text-teal-600 sm:text-4xl md:text-5xl text-2xl",
     },
   ];
   useEffect(() => {
